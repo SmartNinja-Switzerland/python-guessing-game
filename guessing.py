0b58fc3507fa9255f7d0c57a73728be370723763
Peter Bittner <peter@painless.software>
2019-11-11 20:38:56 +0100

    Add error handling (try-except)

--- guessing.py
+++ guessing.py
@@ -10,7 +10,11 @@ attempts = 0
 
 while guess != secret:
     input_text = input(f"Guess the secret number (between {minimum} and {maximum}): ")
-    guess = int(input_text)
+    try:
+        guess = int(input_text)
+    except ValueError:
+        print("Please, enter a number.")
+        continue
     attempts += 1
 
     if guess < minimum or guess > maximum:
@@ -20,20 +24,26 @@ while guess != secret:
     else:
         print(f"Sorry, your guess is not correct... The secret number is not {guess}")
 
-# get current highscore (as a number)
-file = open("hiscore.txt", "r")
-content = file.read()
-hiscore = int(content)
-file.close()
+try:
+    file = open("hiscore.txt", "r")
+    content = file.read()
+    hiscore = int(content)
+    file.close()
+except FileNotFoundError:
+    hiscore = 999_999_999
+except ValueError:
+    print("Highscore file is corrupt.")
+    hiscore = 999_999_999
 
 print(f"It took you {attempts} tries.")
 
-# if better than highscore
 if attempts < hiscore:
-    # say "you are better than last time" if highscore improved
     print("Congratulations! You made a new highscore!")
 
-    file = open("hiscore.txt", "w")
-    text = str(attempts)
-    file.write(text)
-    file.close()
+    try:
+        file = open("hiscore.txt", "w")
+        text = str(attempts)
+        file.write(text)
+        file.close()
+    except IOError as error:
+        print(f"Error writing file: {error}")
